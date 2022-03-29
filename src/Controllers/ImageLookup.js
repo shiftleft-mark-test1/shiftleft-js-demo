28cacf3ef20e1b46d57a76c2b360bafb66857a62
shiftleft-mark-test1 <102538992+shiftleft-mark-test1@users.noreply.github.com>
2022-03-29 10:31:52 -0400

    Update ImageLookup.js

--- src/Controllers/ImageLookup.js
+++ src/Controllers/ImageLookup.js
@@ -11,10 +11,10 @@ class ImageLookup {
     //res.send(fileContent);
   }
   
- /* function sanitizeString(str){
-  str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim,"");
-  return str.trim();
- }*/
+ // function sanitizeString(str){
+ // str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim,"");
+ // return str.trim();
+ //}
 }
 
 module.exports = ImageLookup;
